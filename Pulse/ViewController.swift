f954b2317708dc1ebfa9a57ec6cb0dab4f3a67b3
Manjit Bedi <dev@manjitbedi.com>
2015-12-10 11:54:27 -0800

    Initial Commit

---
+++ Pulse/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pulse
+//
+//  Created by Manjit Bedi on 2015-12-10.
+//  Copyright © 2015 No Org. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
